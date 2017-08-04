9309cc93dbbcfd3c82ab7e6d915b66ef11c8db62
Betty <b.lam4392@gmail.com>
2017-08-03 22:49:02 -0700

    Create class for map data structure

--- w1d5/diy_adts.rb
+++ w1d5/diy_adts.rb
@@ -35,3 +35,41 @@ class Queue
     @queue
   end
 end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    exists = false
+    @map.each_index do |idx|
+      if @map[idx][0] == key
+        @map[idx][1] = value
+        exists = true
+      end
+    end
+    @map << [key, value] unless exists
+    [key, value]
+  end
+
+  def lookup(key)
+    @map.each_index do |idx|
+      return @map[idx][1] if @map[idx][0] == key
+    end
+    nil
+  end
+
+  def remove(key)
+    value = nil
+    @map.each_index do |idx|
+      value = @map[idx][1] if @map[idx][0] == key
+    end
+    return nil unless value
+    @map.delete([key, value])
+  end
+
+  def show
+    @map
+  end
+end
